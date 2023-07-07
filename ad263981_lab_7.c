8702ec4044c21b0742ee192d953ff6d65c112ebf
agreen1738 <136845925+agreen1738@users.noreply.github.com>
2023-07-07 10:30:28 -0400

    Create ad263981_lab_7.c

---
+++ ad263981_lab_7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int a[], int len, int* count)
+{
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < (len - 1); j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                (*count)++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    int len = 9;
+    int count = 0;
+
+    bubbleSort(a, len, &count);
+
+    for (int i = 0; i < len; i++)
+        printf("a[%d] = %d\n", i, a[i]);
+
+    printf("The number of swaps needed to sort were: %d\n", count);
+
+    return 0;
+}
